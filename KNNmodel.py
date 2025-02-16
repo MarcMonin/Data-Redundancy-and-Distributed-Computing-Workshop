a194ec5c92357585b744621cc7c3d9746794544d
MarcMonin <marc@lesmonin.com>
2025-02-16 20:29:07 +0100

    Adding ngrok api ports

--- KNNmodel.py
+++ KNNmodel.py
@@ -1,18 +1,20 @@
-
 import joblib
 from flask import Flask, request, jsonify
 from sklearn.datasets import load_breast_cancer
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
+from pyngrok import ngrok
 
+# Kill any previous ngrok session
+ngrok.kill()
 
 # Charger le dataset
 dataset = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.2, random_state=42)
 
 # Entraîner un modèle
-k=3
+k = 3
 model = KNeighborsClassifier(n_neighbors=k)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
@@ -26,12 +28,15 @@ model = joblib.load("KNeighbors.pkl")
 # Initialiser Flask
 app = Flask(__name__)
 
+#Very unsecure -> complete with your auth token
+ngrok.set_auth_token("")
+
 @app.route('/predict', methods=['GET'])
 def predict():
     try:
-      # Faire une prédiction
+        # Faire une prédiction
         prediction = accuracy_score(y_test, y_pred)
-        
+
         # Retourner le résultat
         return jsonify({"model": "KNeighbors", "prediction": prediction})
 
@@ -39,4 +44,11 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    # Assurez-vous qu'aucune session précédente ne tourne
+    ngrok.kill()
+
+    # Lancer une nouvelle connexion
+    public_url = ngrok.connect(5001).public_url
+    print(f"Public URL: {public_url}")
+
+    app.run(port=5001, debug=False)
